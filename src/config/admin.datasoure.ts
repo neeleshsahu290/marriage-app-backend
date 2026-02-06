5735f52da8c0c774eb01eac5a501cc2f8878e58f
neeleshsahu290 <neeleshsahu290@gmail.com>
2026-02-06 13:25:23 +0530

    update

--- src/config/admin.datasoure.ts
+++ src/config/admin.datasoure.ts
@@ -1,31 +1,29 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import 'dotenv/config'
-import { UserProfile } from "../entity/UserProfile";
+import "dotenv/config";
+
 import { User } from "../entity/User";
+import { UserProfile } from "../entity/UserProfile";
 import { Match } from "../entity/Match";
 import { UserPreference } from "../entity/UserPreference";
 
-
-
 export const AdminDataSource = new DataSource({
   type: "postgres",
-  // host: process.env.SUPABASE_DB_HOST,
-  // port: parseInt(process.env.SUPABASE_DB_PORT || "5432"),
-  // username: process.env.SUPABASE_DB_USER,
-  // password: process.env.SUPABASE_DB_PASSWORD,
-  // database: process.env.SUPABASE_DB_NAME,
 
-    url: process.env.DATABASE_URL,
+  url: process.env.DATABASE_URL,
 
-  ssl: {
-    rejectUnauthorized: false,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false, // ✅ important for Supabase
+    },
   },
 
-  synchronize: true,
+  synchronize: false, // ⚠️ don't use true in prod (can break schema)
+
   logging: false,
 
-  entities: [User, UserProfile,Match,UserPreference],
+  entities: [User, UserProfile, Match, UserPreference],
+
   migrations: [],
   subscribers: [],
 });
