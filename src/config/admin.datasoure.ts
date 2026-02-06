77cc198e916e45daf784fc340be01ed7562f1793
neeleshsahu290 <neeleshsahu290@gmail.com>
2026-02-06 13:03:30 +0530

    update admin database

--- src/config/admin.datasoure.ts
+++ src/config/admin.datasoure.ts
@@ -10,11 +10,13 @@ import { UserPreference } from "../entity/UserPreference";
 
 export const AdminDataSource = new DataSource({
   type: "postgres",
-  host: process.env.SUPABASE_DB_HOST,
-  port: parseInt(process.env.SUPABASE_DB_PORT || "5432"),
-  username: process.env.SUPABASE_DB_USER,
-  password: process.env.SUPABASE_DB_PASSWORD,
-  database: process.env.SUPABASE_DB_NAME,
+  // host: process.env.SUPABASE_DB_HOST,
+  // port: parseInt(process.env.SUPABASE_DB_PORT || "5432"),
+  // username: process.env.SUPABASE_DB_USER,
+  // password: process.env.SUPABASE_DB_PASSWORD,
+  // database: process.env.SUPABASE_DB_NAME,
+
+    url: process.env.DATABASE_URL,
 
   ssl: {
     rejectUnauthorized: false,
